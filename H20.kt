a00d4178fd4b0501a500d8d724ce3b027612f7ca
Elisa <elisa-devops@users.noreply.github.com>
2024-10-10 22:30:05 -0500

    cleaned up while/ if loops to allow for multiple entries

--- H20.kt
+++ H20.kt
@@ -22,51 +22,40 @@ var fluidTracker = 0
 
 fun welcome() {
     println("Welcome to the H20 Tracker. Written by Elisa Aldridge.")
-    println("Would you like to enter water consumption details? y/n")
-    var userInput = readLine()
 
-    if (userInput == "y" || userInput == "n") {
-        while (userInput == "y" || userInput == "n") {
-            if (userInput == "y") {
-                println("Please enter the amout of fluid you consumed in Ounces:")
-                var inputOunces = readLine()!!.toInt()
-                fluidTracker += inputOunces
-                println("You have consumed $fluidTracker Ounces\n")
+    while (true) {
+        println("Would you like to enter water consumption details? y/n")
+        var userInput = readLine()
+        if (userInput == "y") {
+            println("Please enter the amout of fluid you consumed in Ounces:")
+            var inputOunces = readLine()!!.toInt()
+            fluidTracker += inputOunces
+            println("You have consumed $fluidTracker Ounces\n")
 
-                println("Please enter the time you consume the fluid: [example: 3:30pm is 15:30]")
-                var inputTime = readLine()
+            println("Please enter the time you consume the fluid: [example: 3:30pm is 15:30]")
+            var inputTime = readLine()
 
-                println("Please enter the latitude:")
-                var inputLat = readLine()
+            println("Please enter the latitude:")
+            var inputLat = readLine()
 
-                println("Pleae enter the longitude:")
-                var inputLong = readLine()
+            println("Pleae enter the longitude:")
+            var inputLong = readLine()
 
-                inputOfUser.add("$inputOunces,$inputTime,$inputLat,$inputLong")
-                println("$inputOunces,$inputTime,$inputLat,$inputLong")
+            inputOfUser.add("$inputOunces,$inputTime,$inputLat,$inputLong")
+            println("$inputOunces,$inputTime,$inputLat,$inputLong")
 
-                println("Would you like to enter more fluids you consumed? y/n")
-                userInput = readLine()
-                if (userInput == "y") {
-                    println("Please enter the amout of fluid you consumed in Ounces:")
-                    val ounces = readLine()!!.toInt()
-                    fluidTracker += ounces
-                    println("You have consumed $fluidTracker Ounces\n")
-                } else {
-                    println("You hace consumed a total of $fluidTracker Ounces")
-                    break
-                }
-                break
-            } else {
-                println("You selected no\n")
-                println("Goodbye")
-                break
-            }
+            fluidTracker += inputOunces
+            println("You have consumed a total of $fluidTracker Ounces")
+        } else if (userInput == "n") {
+            println("You selected no\n")
+            println("Goodbye")
+            break
+        } else {
+            println("You did not select a valid input\n")
+            println("Good-bye")
+            break
         }
-    }  else {
-        println("You did not select a valid input\n")
-        println("Good-bye")
-    }
+    }  
 }
 
 fun main() {
