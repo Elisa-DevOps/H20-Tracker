fc4cbdfa341e9e73e5d82e44a89fecbdfd93775c
Elisa <elisa-devops@users.noreply.github.com>
2024-10-10 21:57:05 -0500

    csv read/ write and user logic process

--- H20.kt
+++ H20.kt
@@ -8,19 +8,53 @@
     }
 }*/
 
+import java.io.File
+
+fun readCsvSimple(fileName: String) {
+    val lines = File(fileName).readLines()
+    lines.forEach { line ->
+        val row = line.split(",")
+        println(row)
+    }
+}
+
+var inputOfUser = mutableListOf("Ounces,Time,Lat,Long")
+
+fun writeCsvSimple(fileName: String) {
+    /*inputOfUser = mutableListOf(
+        "8, 13:52"
+    )*/
+    File(fileName).writeText(inputOfUser.joinToString("\n"))
+}
+
 var fluidTracker = 0
 
-fun waterConsum() {
+fun welcome() {
+    println("Welcome to the H20 Tracker. Written by Elisa Aldridge.")
     println("Would you like to enter water consumption details? y/n")
     var userInput = readLine()
+
     if (userInput == "y" || userInput == "n") {
         while (userInput == "y" || userInput == "n") {
             if (userInput == "y") {
                 println("Please enter the amout of fluid you consumed in Ounces:")
-                val ounces = readLine()!!.toInt()
-                fluidTracker += ounces
+                var inputOunces = readLine()!!.toInt()
+                fluidTracker += inputOunces
                 println("You have consumed $fluidTracker Ounces\n")
-                println("Would you like to enter more fluids you consumed?")
+
+                println("Please enter the time you consume the fluid: [example: 3:30pm is 15:30]")
+                var inputTime = readLine()
+
+                println("Please enter the latitude:")
+                var inputLat = readLine()
+
+                println("Pleae enter the longitude:")
+                var inputLong = readLine()
+
+                inputOfUser.add("$inputOunces,$inputTime,$inputLat,$inputLong")
+                println("$inputOunces,$inputTime,$inputLat,$inputLong")
+
+                println("Would you like to enter more fluids you consumed? y/n")
                 userInput = readLine()
                 if (userInput == "y") {
                     println("Please enter the amout of fluid you consumed in Ounces:")
@@ -39,11 +73,17 @@ fun waterConsum() {
             }
         }
     }  else {
-        println("you did not select a valid input\n")
+        println("You did not select a valid input\n")
         println("Good-bye")
     }
 }
 
 fun main() {
-    waterConsum()
+    //writeCsvSimple("water.csv")
+    //readCsvSimple("water.csv")
+    //inputOfUser.add("9,2:54")
+    welcome()
+    writeCsvSimple("water.csv")
+    readCsvSimple("water.csv")
+
 }
